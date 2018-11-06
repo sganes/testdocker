630b365f426556a155586a017b48a97e44073448
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-06 13:03:41 +0100

    changes in mongo db

--- mongodb_connect.js
+++ mongodb_connect.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb://mongo:27017'
+const uri = 'mongodb://0.0.0.0:27017'
 console.log(uri)
 module.exports = function(callback) {
   MongoClient.connect(uri,{ useNewUrlParser: true },(err,client) => {
