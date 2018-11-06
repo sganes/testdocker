17ed59b49edcaa1241c1d4ea2cf0306f0aa14e0c
shreeganes <shreevidhya.ganesan@byon8.com>
2018-11-06 12:36:16 +0100

    new commit

--- mongodb_connect.js
+++ mongodb_connect.js
@@ -1,8 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = 'mongo:27017/DockerTest';
-mongoose.connect('database:27017/mongocrud')
 console.log(uri)
-
 module.exports = function(callback) {
   MongoClient.connect(uri,{ useNewUrlParser: true },(err,client) => {
       if(err)
